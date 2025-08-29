bb76bd602f65d69d75cfadd4138f747eb3047a68
Leonardo Maciel <leonardomacielj@gmail.com>
2025-08-29 13:59:45 -0300

    feat: create menu sideBar

---
+++ src/components/MenuSideBar.tsx
@@ -0,0 +1,16 @@
+import type { IconType } from "react-icons";
+
+interface MenuSideBarProps {
+  icon?: IconType;
+}
+
+const MenuSideBar = ({ icon: Icon }: MenuSideBarProps) => {
+  return (
+    <div className="flex items-center justify-center bg-white p-2 rounded-xl relative group cursor-pointer">
+      <span className="hidden w-1 h-full bg-white absolute -left-2.5 group-hover:block" />
+      {Icon && <Icon color="#649fbf" size={25} />}
+    </div>
+  );
+};
+
+export default MenuSideBar;
