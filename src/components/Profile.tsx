c49e719ccb9d9b3af424fd2d17fa7de19a0a52bc
Leonardo Maciel <leonardomacielj@gmail.com>
2025-08-29 15:29:34 -0300

    feat: create component to profile user

---
+++ src/components/Profile.tsx
@@ -0,0 +1,24 @@
+import user from "./../assets/user.svg";
+
+const Profile = () => {
+  return (
+    <div className="bg-white flex flex-col p-6 gap-8 max-w-[500px] rounded-3xl shadow-lg">
+      <p className="text-medium-gray">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In suscipit
+        suscipit porttitor. Suspendisse ex lorem, rhoncus nec ante eu, venenatis
+        aliquam turpis. Nulla facilisi. Curabitur nec mattis dolor. Nulla
+        finibus bibendum ligula tempus vehicula. Ut at tristique libero, nec
+        efficitur dui. Aliquam erat volutpat. Fusce quam sem, tempus nec justo
+        eget, luctus scelerisque velit. Nam sollicitudin purus urna, vitae
+        ornare neque tincidunt vel. Proin ac lacinia erat, et commodo felis.
+        Phasellus tempor tellus eu vulputate tempus.
+      </p>
+
+      <div>
+        <img src={user} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
