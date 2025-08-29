b0aedcf06ef686271a9d0818b4f7f7f8765bd1d4
Leonardo Maciel <leonardomacielj@gmail.com>
2025-08-29 14:57:08 -0300

    style: centralized item text

--- src/components/stages/components/Item.tsx
+++ src/components/stages/components/Item.tsx
@@ -5,10 +5,10 @@ const ItemStage = ({ stage }: { stage: number }) => {
     <>
       <div className="space-y-2 relative">
         <div className="rounded-2xl bg-blue-default w-16 h-16 flex justify-center items-center shadow-md shadow-zinc-200 hover:shadow-zinc-500 hover:border-2 border-black cursor-pointer">
-          <FaBuilding color="white" size={32} />
+          <FaBuilding color="white" size={30} />
         </div>
 
-        <p className="text-center text-blue-default font-semibold absolute -bottom-5 left-2">
+        <p className="text-center text-d text-blue-default font-medium absolute -bottom-5 left-2.5">
           Item {stage}
         </p>
       </div>
