9d764d286df19ed4c25bf6b7bed2879d7a9e53e6
Leonardo Maciel <leonardomacielj@gmail.com>
2025-08-29 14:00:01 -0300

    feat: create sideBar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,7 +1,26 @@
+import {
+  FaEdit,
+  FaRegBuilding,
+  FaNetworkWired,
+  FaRegBell,
+  FaUserAlt,
+} from "react-icons/fa";
+import MenuSideBar from "./MenuSideBar";
+import { FaClockRotateLeft } from "react-icons/fa6";
+
 const SideBar = () => {
   return (
-    <div>
-      <span>teste</span>
+    <div className="bg-blue-default w-16 h-screen rounded-tr-2xl py-10 flex items-center justify-center relative">
+      <div className="w-full h-10 bg-white absolute top-10" />
+
+      <div className="flex flex-col gap-6 items-center justify-center">
+        <MenuSideBar icon={FaRegBuilding} />
+        <MenuSideBar icon={FaEdit} />
+        <MenuSideBar icon={FaNetworkWired} />
+        <MenuSideBar icon={FaRegBell} />
+        <MenuSideBar icon={FaClockRotateLeft} />
+        <MenuSideBar icon={FaUserAlt} />
+      </div>
     </div>
   );
 };
