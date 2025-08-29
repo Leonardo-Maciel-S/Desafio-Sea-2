4a5b2ad93d8dcc06f7459da0a16b661e1ca49717
Leonardo Maciel <leonardomacielj@gmail.com>
2025-08-29 15:28:47 -0300

    style: add shadow in stages component

--- src/components/stages/Stages.tsx
+++ src/components/stages/Stages.tsx
@@ -4,7 +4,7 @@ const Stages = () => {
   const stagesList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   return (
-    <div className="w-full flex items-center bg-white max-h-36 rounded-xl p-6">
+    <div className="w-full flex items-center bg-white max-h-36 rounded-xl p-6 pb-10 shadow-lg">
       {stagesList.map((stage) => (
         <ItemStage stage={stage} />
       ))}
