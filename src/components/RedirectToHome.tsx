360467baea9fc3d68140c5c8c7f17da8f72977fb
Leonardo Maciel <leonardomacielj@gmail.com>
2025-08-30 11:58:36 -0300

    feat: redirect to /employee when access / route

---
+++ src/components/RedirectToHome.tsx
@@ -0,0 +1,7 @@
+import { Navigate } from "react-router";
+
+const RedirectToEmployee = () => {
+  return <Navigate to="employee" />;
+};
+
+export default RedirectToEmployee;
--- src/routes/router.ts
+++ src/routes/router.ts
@@ -2,12 +2,17 @@ import { createBrowserRouter } from "react-router";
 
 import App from "../App";
 import Employee from "../pages/Employee";
+import RedirectToEmployee from "../components/RedirectToHome";
 
 export const router = createBrowserRouter([
   {
     path: "",
     Component: App,
     children: [
+      {
+        path: "",
+        Component: RedirectToEmployee,
+      },
       {
         path: "employee",
         Component: Employee,
