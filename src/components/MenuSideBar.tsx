613cae19f3aaf8078b81e8d80a6d5565d69eaa13
Leonardo Maciel <leonardomacielj@gmail.com>
2025-08-30 13:07:51 -0300

    feat: add Links path to MenuSideBars

--- src/components/MenuSideBar.tsx
+++ src/components/MenuSideBar.tsx
@@ -1,15 +1,35 @@
 import type { IconType } from "react-icons";
+import { Link } from "react-router";
 
 interface MenuSideBarProps {
   icon?: IconType;
+  destinationPath: string;
+  actualPath: string;
 }
 
-const MenuSideBar = ({ icon: Icon }: MenuSideBarProps) => {
+const MenuSideBar = ({
+  icon: Icon,
+  destinationPath,
+  actualPath,
+}: MenuSideBarProps) => {
+  const isSelected = destinationPath === actualPath;
+
   return (
-    <div className="flex items-center justify-center bg-gray-default hover:bg-white p-2 rounded-xl relative group cursor-pointer">
-      <span className="hidden w-1 h-full bg-white absolute -left-3 group-hover:block" />
-      {Icon && <Icon color="#649fbf" size={25} />}
-    </div>
+    <Link to={destinationPath}>
+      <button
+        className={`flex items-center justify-center p-2 rounded-xl relative group cursor-pointer ${
+          isSelected ? "bg-white" : "bg-gray-default hover:bg-white"
+        }`}
+        disabled={isSelected}
+      >
+        <span
+          className={`w-1 h-full bg-white absolute -left-3 group-hover:block ${
+            isSelected ? "block" : "hidden"
+          }`}
+        />
+        {Icon && <Icon color="#649fbf" size={25} />}
+      </button>
+    </Link>
   );
 };
 
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -7,19 +7,46 @@ import {
 } from "react-icons/fa";
 import MenuSideBar from "./MenuSideBar";
 import { FaClockRotateLeft } from "react-icons/fa6";
+import { useLocation } from "react-router";
 
 const SideBar = () => {
+  const { pathname } = useLocation();
+
   return (
     <div className="bg-blue-default w-18 h-screen rounded-r-2xl py-10 flex items-center justify-center relative">
       <div className="w-full h-10 bg-white absolute top-10" />
 
       <div className="flex flex-col gap-6 items-center justify-center">
-        <MenuSideBar icon={FaRegBuilding} />
-        <MenuSideBar icon={FaEdit} />
-        <MenuSideBar icon={FaNetworkWired} />
-        <MenuSideBar icon={FaRegBell} />
-        <MenuSideBar icon={FaClockRotateLeft} />
-        <MenuSideBar icon={FaUserAlt} />
+        <MenuSideBar
+          icon={FaRegBuilding}
+          destinationPath={"/home"}
+          actualPath={pathname}
+        />
+        <MenuSideBar
+          icon={FaEdit}
+          destinationPath={"/employee"}
+          actualPath={pathname}
+        />
+        <MenuSideBar
+          icon={FaNetworkWired}
+          destinationPath={"/network"}
+          actualPath={pathname}
+        />
+        <MenuSideBar
+          icon={FaRegBell}
+          destinationPath={"/notification"}
+          actualPath={pathname}
+        />
+        <MenuSideBar
+          icon={FaClockRotateLeft}
+          destinationPath={"/updates"}
+          actualPath={pathname}
+        />
+        <MenuSideBar
+          icon={FaUserAlt}
+          destinationPath={"/user"}
+          actualPath={pathname}
+        />
       </div>
     </div>
   );
